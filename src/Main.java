2ed40c387c1b2917e69c1f235969d052fcd2e58a
Miguel Avila Luperi Perez Gomez <migueltkd63@gmail.com>
2026-04-14 15:04:40 -0300

    Desafio Alura Banco

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String nome = "Miguel Avila Luperi Perez Gomez";
+        String tipoConta = "Corrente";
+        Double Saldo = 2500.0;
+        int escolha = 0;
+        while(escolha != 4){
+            System.out.println("1- Consultar saldos");
+            System.out.println("2- Receber valor");
+            System.out.println("3- Transferir valor");
+            System.out.println("4- Sair");
+            escolha = scanner.nextInt();
+
+
+            switch (escolha){
+                case 1 :
+                    System.out.println(Saldo);
+                    System.out.println("");
+                    break;
+                case 2 :
+                    System.out.println("Digite o Valor a receber");
+                    double valor = scanner.nextDouble();
+                    Saldo += valor;
+                    break;
+                case 3 :
+                    System.out.println("Digite o valor a ser transferido");
+                    double transferido = scanner.nextDouble();
+                    Saldo -= transferido;
+                    break;
+
+
+            }
+
+        }
+    }
+}
